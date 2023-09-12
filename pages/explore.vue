51391f9e8049d6021620ead3cc23a4ff7abad7b9
Machive5 <machiblue5@gmail.com>
2023-09-12 19:22:36 +0700

    fixing explore bug

--- pages/explore.vue
+++ pages/explore.vue
@@ -23,8 +23,8 @@
             <div class="content row row-cols-auto g-4">
                 <div v-for="(dt,id) in show" :key="id" class="col">
                     <div class="card shadow" :style="{width: 'calc(8rem + 6vw)', height:'100%'}">
-                        <div class="wrap" style="height:45%;">
-                            <img src="@/src/img/Screenshot_5.png" class="card-img-top" alt="img" :style="{width:'100%', height:'100%', objectFit:'cover'}">
+                        <div class="wrap" style="height: 45%; width: 100%; overflow: hidden;">
+                            <img :src="dt.cover" width="200" height="110" class="card-img-top" alt="img" :style="{objectFit: 'cover'}">
                         </div>
                         <div class="card-body">
                             <h5 class="card-title">{{ dt.title }}</h5>
@@ -50,30 +50,35 @@
             'content': 'Sint cupidatat cupidatat nulla aute aliqua quis nostrud duis tempor eu in. Labore commodo ad anim qui excepteur ea irure amet deserunt non mollit commodo. Adipisicing consequat eiusmod officia culpa elit anim id aute ex est commodo id fugiat dolore. Ea eu minim minim sit culpa qui laborum pariatur veniam aute nisi proident velit occaecat. Qui labore eiusmod aute elit. Irure laborum sit nostrud incididunt sit ipsum ex aute incididunt laborum.',
             'author': 'Pina',
             'categories': ['Folk Lore',],
+            'cover': require("@/src/img/timun-mas.jpg"),
         },
         {
             'title': 'Kuntilanak',
             'content': 'Esse occaecat duis est enim voluptate exercitation id amet velit voluptate. Dolore ex elit eiusmod id. Tempor eu incididunt non occaecat. Elit laborum officia magna velit voluptate amet magna mollit commodo nisi cupidatat adipisicing do dolore.',
             'author': 'Tirto Adhi',
             'categories': ['Horror',],
+            'cover': require("@/src/img/kunti.jpg"),
         },
         {
             'title': 'Bedawang',
             'content': 'Minim laboris adipisicing aute tempor aliquip ut enim. Dolore quis cupidatat proident occaecat dolor consectetur. Nostrud irure Lorem deserunt cillum Lorem. Qui reprehenderit ut reprehenderit consectetur ad nostrud deserunt cillum reprehenderit aliquip eiusmod. Lorem culpa consequat consequat Lorem officia reprehenderit duis aliqua et quis in laboris dolore. Adipisicing esse Lorem deserunt cupidatat sunt occaecat voluptate eu esse occaecat. Aliquip et consequat eu esse et non velit dolore Lorem aute id excepteur adipisicing aliquip.',
             'author': 'Kaluna',
             'categories': ['Myth',],
+            'cover': require("@/src/img/bedawang.jpg"),
         },
         {
             'title': 'Palapa',
             'content': 'Minim laboris adipisicing aute tempor aliquip ut enim. Dolore quis cupidatat proident occaecat dolor consectetur. Nostrud irure Lorem deserunt cillum Lorem. Qui reprehenderit ut reprehenderit consectetur ad nostrud deserunt cillum reprehenderit aliquip eiusmod. Lorem culpa consequat consequat Lorem officia reprehenderit duis aliqua et quis in laboris dolore. Adipisicing esse Lorem deserunt cupidatat sunt occaecat voluptate eu esse occaecat. Aliquip et consequat eu esse et non velit dolore Lorem aute id excepteur adipisicing aliquip.',
             'author': 'Rama',
             'categories': ['History',],
+            'cover': require("@/src/img/palapa.jpg"),
         },
         {
             'title': 'Roro Jongrang',
             'content': 'Minim reprehenderit sit labore eu aute non fugiat. Ipsum cillum ex eu irure exercitation aliqua deserunt duis exercitation elit labore officia ad. Reprehenderit et sint minim incididunt occaecat Lorem ex in magna nulla nulla sint. Irure incididunt culpa qui incididunt eiusmod id cupidatat. Mollit elit nostrud dolore deserunt.',
             'author': 'Rama',
             'categories': ['Myth', 'Legend'],
+            'cover': require("@/src/img/roro-jonggrang.jpg"),
         },
     ]
 
@@ -135,7 +140,7 @@
     }
     @font-face {
         font-family: 'boutiques of merauke';
-        src: url("@/src/font/boutiques-of-merauke-font/BoutiquesOfMerauke.ttf");
+        src: url("~/src/font/boutiques-of-merauke-font/BoutiquesOfMerauke.ttf");
     }
     .main{
         box-sizing: border-box;
@@ -147,16 +152,21 @@
         margin: 20px 0px;
         padding: 0px 15%;
         height: auto;
-        height: auto;
         justify-content: center;
         background-color: rgb(226, 223, 223);
-    }
-    img.car-img-top{
-        height: 45%;
-        object-fit: cover;
+        position: relative;
     }
     .content.row h1{
         font-size: calc(1.375rem + 6vw);
+    }
+    .col{
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        margin: 15px 0px;
+        text-align: center;
     }
     .card-title{
         font-size: calc(0.45rem + 1vw);
@@ -170,13 +180,8 @@
     .preview{
         text-align: center;
     }
-    .col{
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-        margin: 15px 0px;
-        text-align: center;
+    .card-body{
+        height: min-content;
     }
     .box{
         display: flex;
@@ -211,7 +216,7 @@
             padding: 0px 8%;
         }
     }
-    @media only screen and (max-width: 525px){
+    @media only screen and (max-width: 440px){
         .card{
             display: flex;
             flex-direction: row;
@@ -224,12 +229,16 @@
             margin: 0px;
         }
         .wrap{
-            min-width: 106px;
+            min-width: 80px;
             width: 30% !important;
             height: 100% !important ;
+            display: flex;
+            justify-content: center;
+            align-items: center;
         }
         .card-body{
-            width: calc(2rem + 55%) !important;
+            width: 100% !important;
+            overflow: hidden;
             position: relative;
         }
         img.card-img-top{
@@ -247,6 +256,9 @@
         }
         .preview{
             text-align: left;
+            height: auto;
+            max-height: 27px;
+            overflow: hidden;
         }
         .col{
             margin: 8px 0px;
---
+++ pages/write.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <NavbarTemplate />
+        <div class="main">
+            <div v-if="controllPanel" class="controllPanel">
+                <h1 class="heading1">Heading 1</h1>
+                <h2 class="heading2">Heading 2</h2>
+                <h3 class="heading3">Heading 3</h3>
+                <p class="paragraph">Paragraph</p>
+                <img src="" alt="image" class="image">
+            </div>
+            <div class="appearance container-fluid">
+                <div class="cover">
+                    <input type="file" name="" id="" value="Cover">
+                </div>
+                <div>
+                    <h1 contenteditable="true"></h1>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="true">
+                            Filter categories
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><div class="dropdown-item"><input type="checkbox" name="All" id="All"><label for="All">All</label></div></li>
+                            <li><div class="dropdown-item"><input type="checkbox" name="Folk Lore" id="Folk"><label for="Folk">Folk Lore</label></div></li>
+                            <li><div class="dropdown-item"><input type="checkbox" name="Horror" id="Horror"><label for="Horror">Horror</label></div></li>
+                            <li><div class="dropdown-item"><input type="checkbox" name="History" id="History"><label for="History">History</label></div></li>
+                            <li><div class="dropdown-item"><input type="checkbox" name="Legend" id="Legend"><label for="Legend">Legend</label></div></li>
+                            <li><div class="dropdown-item"><input type="checkbox" name="Myth" id="Myth"><label for="Myth">Myth</label></div></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+
+            </div>
+            <div class="controller" @click="cpanel">
+                <h1>add components</h1>
+            </div>
+        </div>
+        <FooterTemplate />
+    </div>
+</template>
+<script>
+    import FooterTemplate from '~/components/FooterTemplate.vue'
+    import NavbarTemplate from '~/components/NavbarTemplate.vue'
+
+    export default {
+        components:{
+            NavbarTemplate,
+            FooterTemplate,
+        },
+        data() {
+            return {
+                controllPanel: false,
+            }
+        },
+        mounted() {
+
+        },
+        computed:{
+            
+        },
+        methods:{
+            cpanel(){
+                this.controllPanel = !this.controllPanel;
+            },
+        }
+    }
+</script>
+<style>
+    .dropdown{
+        align-self: flex-start;
+        justify-self: flex-start;
+    }
+    .dropdown-item{
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        margin: 0%;
+    }
+    label{
+        width: 100%;
+        margin-left: 4%;
+    }
+    .appearance{
+        display: flex;
+        flex-direction: row;
+    }
+</style>
